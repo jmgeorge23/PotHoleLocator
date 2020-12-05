c6da9d5c25cbe9e7fb21b78868cee9a821bb9ac7
Ben Squier <squier.ben@gmail.com>
2020-12-05 13:05:31 -0500

    Google maps Map added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,5 +5,27 @@
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
+    <Map/>
   </div>
 </template>
+
+<script>
+import Map from './components/Map';
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import VueGeolocation from 'vue-browser-geolocation'
+
 
 Vue.config.productionTip = false
+Vue.use(VueGeolocation)
+
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCt2Xrz7cjy3dyahlfegbOmyZ4vrTJyOL4'
+  }
+})
 
 new Vue({
   router,
