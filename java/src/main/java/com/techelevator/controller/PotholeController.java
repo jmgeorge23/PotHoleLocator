034d8d8692c4ae9d7d39ca73abcb83bd48df8586
Ben Squier <squier.ben@gmail.com>
2020-12-07 11:39:45 -0500

    Fleshed out Pothole Controller

--- java/src/main/java/com/techelevator/controller/PotholeController.java
+++ java/src/main/java/com/techelevator/controller/PotholeController.java
@@ -1,11 +1,48 @@
 package com.techelevator.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.dao.PotholeDAO;
+import com.techelevator.model.Pothole;
+import com.techelevator.model.PotholeDTO;
 
 @RestController
 public class PotholeController {
+	
+	private PotholeDAO potholeDAO;
 
-	public PotholeController() {
-		
+	public PotholeController(PotholeDAO potholeDAO) {
+		this.potholeDAO = potholeDAO;
 	}
+	
+//	@RequestMapping(value = "/potholes", method = RequestMethod.GET)
+//	public List<Pothole> listAllPotholes() {
+//		return potholeDAO.findAllPotholes();
+//	}
+	
+//	@RequestMapping(value = "/potholes/{id}", method = RequestMethod.GET)
+//	public Pothole getPothole(@PathVariable int id) {
+//		return potholeDAO.getPothole(id);
+//	}
+	
+//	@ResponseStatus(HttpStatus.CREATED)
+//	@RequestMapping(value = "/potholes", method = RequestMethod.POST)
+//	public void addPothole(@Valid @RequestBody PotholeDTO newPothole) {
+//		potholeDAO.createPothole(newPothole);
+//	}
+	
+//	@ResponseStatus(HttpStatus.OK)
+//	@RequestMapping(value = "/potholes/{id}", method = RequestMethod.DELETE)
+//	public void deletelPothole(@PathVariable int id) {
+//		potholeDAO.deletePothole(id);
+//	}
 }
