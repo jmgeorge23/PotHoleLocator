a153d8233bf7bcb26de0f84e404f77f9a68480fb
Ben Squier <squier.ben@gmail.com>
2020-12-08 16:08:59 -0500

    Added test potholes

--- java/database/schema-insert-1.sql
+++ java/database/schema-insert-1.sql
@@ -8,6 +8,7 @@ INSERT INTO pothole_status (status) Values('Deleted');
 INSERT INTO severity (severity) Values ('Low');
 INSERT INTO severity (severity) Values ('Medium');
 INSERT INTO severity (severity) Values ('High');
+INSERT INTO severity (severity) Values ('Not Inspected');
 
 
 COMMIT;
---
+++ java/database/scheme-insert-2.sql
@@ -0,0 +1,14 @@
+Begin;
+
+INSERT INTO potholes (pothole_id, pothole_status_id, severity_id, lat, lng) VALUES(DEFAULT, 1, 4, 39.9451, -75.1702);
+INSERT INTO potholes (pothole_id, pothole_status_id, severity_id, lat, lng) VALUES(DEFAULT, 1, 4, 39.9468, -75.1666);
+INSERT INTO potholes (pothole_id, pothole_status_id, severity_id, lat, lng) VALUES(DEFAULT, 1, 4, 39.9594, -75.1499);
+INSERT INTO potholes (pothole_id, pothole_status_id, severity_id, lat, lng) VALUES(DEFAULT, 1, 4, 39.9654, -75.1660);
+
+
+INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES(18, 1, 4, 39.9451, -75.1702, CURRENT_TIMESTAMP);
+INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES(19, 1, 4, 39.9468, -75.1666, CURRENT_TIMESTAMP);
+INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES(20, 1, 4, 39.9594, -75.1499, CURRENT_TIMESTAMP);
+INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES(21, 1, 4, 39.9654, -75.1660, CURRENT_TIMESTAMP);
+
+Commit;
