42a5b25cf83f635e755955d327dae0ec41b7fa6f
Ben Squier <squier.ben@gmail.com>
2020-12-08 16:27:50 -0500

    Fixed error in insert

--- java/database/scheme-insert-2.sql
+++ java/database/scheme-insert-2.sql
@@ -7,7 +7,7 @@ INSERT INTO potholes (pothole_id, pothole_status_id, severity_id, lat, lng) VALU
 
 
 INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES((Select pothole_id From potholes where lat = 39.9451), 1, 4, 39.9451, -75.1702, CURRENT_TIMESTAMP);
-INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES((Select pothole_id From potholes where lat = 39.9468), 1, 4, 39.9468), -75.1666, CURRENT_TIMESTAMP);
+INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES((Select pothole_id From potholes where lat = 39.9468), 1, 4, 39.9468, -75.1666, CURRENT_TIMESTAMP);
 INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES((Select pothole_id From potholes where lat = 39.9594), 1, 4, 39.9594, -75.1499, CURRENT_TIMESTAMP);
 INSERT INTO potholes_history (pothole_id, pothole_status_id, severity_id, lat, lng, datetime) VALUES((Select pothole_id From potholes where lat = 39.9654), 1, 4, 39.9654, -75.1660, CURRENT_TIMESTAMP);
 
