a735811344077a82d670f2b6b12eb7a46952259c
Ben Squier <squier.ben@gmail.com>
2020-12-08 15:26:47 -0500

    Made values for status and severity tables

---
+++ java/database/schema-insert-1.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+
+INSERT INTO pothole_status (status) Values('Inspected');
+INSERT INTO pothole_status (status) Values('Reported');
+INSERT INTO pothole_status (status) Values('Completed');
+INSERT INTO pothole_status (status) Values('Deleted');
+
+INSERT INTO severity (severity) Values ('Low');
+INSERT INTO severity (severity) Values ('Medium');
+INSERT INTO severity (severity) Values ('High');
+
+
+COMMIT;
