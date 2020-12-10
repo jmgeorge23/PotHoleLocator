a486a402f9f811ec2df2991e4414101229c35a18
Chris Perez <christopher12perez6@gmail.com>
2020-12-10 17:26:51 -0500

    Claims table fix pt 2

--- java/database/schema-update-6.sql
+++ java/database/schema-update-6.sql
@@ -2,5 +2,9 @@ BEGIN;
 ALTER TABLE claims
   DROP COLUMN amount;
 ALTER TABLE claims
-  ADD COLUMN amount numeric(10,2) NOT NULL;
+  ADD COLUMN amount numeric(10,2);
+ALTER TABLE claims_history
+  DROP COLUMN amount;
+ALTER TABLE claims_history
+  ADD COLUMN amount numeric(10,2);
 COMMIT;
