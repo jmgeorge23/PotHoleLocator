ac1345d142432a6e1b6e09fc69a4c6ea8ee257e3
Chris Perez <christopher12perez6@gmail.com>
2020-12-17 10:33:44 -0500

    Employee dash fix

--- vue/src/components/EmployeeDash.vue
+++ vue/src/components/EmployeeDash.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
   
-  <employee-pothole-list :setHeight="'max-height: 59vh'"/>
+  <employee-pothole-list :setHeight="'max-height: 73vh'"/>
+  <v-btn
+    class="ma-2"
+    outlined
+    color="secondary darken-2"
+  >
+    Submit a Damage Claim
+  </v-btn>
 </div>
   
 </template>
